1f722603bd1c0cee21042f954e6ac668fe548c33
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-08-22 09:24:13 -0600

    Add project files.

---
+++ FileTestForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace FileTest
+{
+    partial class FileTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(24, 88);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(432, 552);
+            this.outputTextBox.TabIndex = 0;
+            this.outputTextBox.Text = "";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.inputTextBox.Location = new System.Drawing.Point(24, 32);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(432, 33);
+            this.inputTextBox.TabIndex = 1;
+            this.inputTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputTextBox_KeyDown);
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputLabel.Location = new System.Drawing.Point(24, 8);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(152, 20);
+            this.inputLabel.TabIndex = 2;
+            this.inputLabel.Text = "Input file or directory";
+            // 
+            // FileTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 661);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.outputTextBox);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "FileTestForm";
+            this.Text = "File Test Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label inputLabel;
+    }
+}
+
---
+++ FileTestForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileTest
+{
+    public partial class FileTestForm : Form
+    {
+        public FileTestForm()
+        {
+            InitializeComponent();
+        }
+
+
+        // Invoked when user presses key
+        private void inputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Determine whether user pressed Enter key
+            if (e.KeyCode == Keys.Enter)
+            {
+                // Get user-specified file dr directory
+                string fileName = inputTextBox.Text;
+
+                // Determine whether the fileName is a file
+                if (File.Exists(fileName))
+                {
+                    // Get file's creation date, modification date, etc.
+                    GetInformation(fileName);
+
+                    // Display file contents through StreamReader 
+                    try
+                    {
+                        // Obtain reader and file contents
+                        using (var stream = new StreamReader(fileName))
+                        {
+                            outputTextBox.AppendText(stream.ReadToEnd());
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error reading from file", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                // Determine whether fileName is a directory 
+                else if (Directory.Exists(fileName))
+                {
+                    // Get directory's cretion date, modification date, etc.
+                    GetInformation(fileName);
+
+                    // Obtain directory list of specified directory 
+                    string[] directoryList = Directory.GetDirectories(fileName);
+
+                    outputTextBox.AppendText("Directory contents:\n");
+
+                    // Output directoryList contents
+                    foreach (var directory in directoryList)
+                    {
+                        outputTextBox.AppendText($"{directory}\n");
+                    }
+                }
+                else
+                {
+                    // Notify user that neither file nor directory exists
+                    MessageBox.Show($"{inputTextBox.Text} does not exist", "File error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Get information on file or directory and output it to outputTextBox
+        private void GetInformation(string fileName)
+        {
+            outputTextBox.Clear();
+
+            // Output that file or directory exists
+            outputTextBox.AppendText($"{fileName} exists\n");
+
+            // Output when file or directory was crated 
+            outputTextBox.AppendText($"Created: {File.GetCreationTime(fileName)}\n" +
+                Environment.NewLine);
+
+            // Output when file or directory was last modified 
+            outputTextBox.AppendText($"Last modified: {File.GetLastWriteTime(fileName)}\n" +
+                Environment.NewLine);
+
+            // Output when file or directory was last accessed
+            outputTextBox.AppendText($"Last accessed: {File.GetLastAccessTime(fileName)}\n" +
+                Environment.NewLine);
+        }
+    }
+}
